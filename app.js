35f5be9349ff70b4844cb4741e1bf7cdca1f56c9
Sonal Agrawal <sonal.agrawal@ridecell.com>
2024-10-06 16:56:08 +0530

    .

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  this_causes_error
+app.listen(PORT + 1, () => {
   console.log(`server started on port ${PORT}`)
 })
 
