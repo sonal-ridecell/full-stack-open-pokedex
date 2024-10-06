5491718dfcc80b156c8ee646f14ea7408cdaf8e9
Sonal Agrawal <sonal.agrawal@ridecell.com>
2024-10-06 16:59:52 +0530

    add health check

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
